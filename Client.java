fd9ea68b3285b628d1c900ef4a2e9857d7154381
klassCode <klassenedmond7@gmail.com>
2019-05-26 15:31:31 -0400

    Ajout du fichier Client.java et du fichier README du projet

---
+++ Client.java
@@ -0,0 +1,52 @@
+public class Client{
+	
+	private String nom;
+	private String prenom;
+	private String telephone;
+	private String adresse;
+	private String email;
+	
+	public Client(){
+		
+	}
+	public Client(String nom, String prenom, String telephone, String adresse, String email){
+		this.nom=nom;
+		this.prenom=prenom;
+		this.telephone= telephone;
+		this.adresse= adresse;
+		this.email= email;
+	}
+	public void afficheClient(){
+		System.out.println("Nom "+ this.nom+"\nPrenom "+this.prenom+"\nTelephone "+this.telephone+"\nAdresse "+ this.adresse+ "\nMail "+ this.email);
+	}
+	public String getNom(){
+		return this.nom;
+	}
+	public String getPrenom(){
+		return this.prenom;
+	}
+	public String getTelephone(){
+		return this.telephone;
+	}
+	public String getAdresse(){
+		return this.adresse;
+	}
+	public String getEmail(){
+		return this.email;
+	}
+	public void setNom(String nom){
+		this.nom=nom;
+	}
+	public void setPrenom(String prenom){
+		this.prenom=prenom;
+	}
+	public void setTelephone(String telephone){
+		this.telephone=telephone;
+	}
+	public void setAdresse(String adresse){
+		this.adresse=adresse;
+	}
+	public void setEmail(String email){
+		this.email=email;
+	}
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Cet projet est ecrit en java et presente un tout petit programme qui presente 
+l'achat d'un produit par un client
+un fichier Client.java
+un fichier Produit.java
+et le fichier main pour faire marcher tout Ca.
