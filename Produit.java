4e8ab6d323cd196109a29ebef4baab63c396f4e1
klassCode <klassenedmond7@gmail.com>
2019-05-26 15:46:53 -0400

    Ajout des getters et setters de la classe Produit

--- Produit.java
+++ Produit.java
@@ -16,4 +16,25 @@ public class Produit{
 	public void afficheClient(){
 		System.out.println("Code "+ this.code+"\nNom"+this.nom+"\nCategorie "+this.categorie+"\n Prix "+ this.prix);
 	}
+	public int getCode(){
+		return code;
+	}
+	public String getNom(){
+		return nom;
+	}
+	public String getCategorie(){
+		return categorie;
+	}
+	public double getPrix(){
+		return prix;
+	}
+	public void setNom(String nom){
+		this.nom= nom;
+	}
+	public void setCategorie(String categorie){
+		this.categorie= categorie;
+	}
+	public void setPrix(double prix){
+		this.prix= prix;
+	}
 }
